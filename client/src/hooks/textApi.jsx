85c96d4f6823dc3bec3bb6c87b082fec726f42c7
Leonardo <kjamanba@hotmail.com>
2023-04-14 12:52:27 -0300

    release-1.1

--- client/src/hooks/textApi.jsx
+++ client/src/hooks/textApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/"
+  baseURL: "https://personal-blog-server-ten.vercel.app/"
 });
 
 export const textApi = () => ({
